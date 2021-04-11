6f4572a973c0e480cc660293cccbe53df7c2e33b
MasatakaIshii <masaradio78@gmail.com>
2021-04-12 00:28:01 +0200

    Add integration test of media controller

--- src/main/java/com/gotta_watch_them_all/app/core/entity/Media.java
+++ src/main/java/com/gotta_watch_them_all/app/core/entity/Media.java
@@ -1,11 +1,19 @@
 package com.gotta_watch_them_all.app.core.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
+@JsonDeserialize(builder = Media.MediaBuilder.class)
+@Builder(builderClassName = "MediaBuilder", toBuilder = true)
 public class Media {
     private Long id;
     private String name;
+
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class MediaBuilder {
+
+    }
 }

17d9b5175a25fe0d029d8ec4ad3f1d7d893edbd3
JeremTer <ternisien09@gmail.com>
2021-05-01 00:05:10 +0200

    Refacto ApiRequestBuilderMovieDb

--- src/main/java/com/gotta_watch_them_all/app/infrastructure/util/ApiRequestBuilder.java
+++ src/main/java/com/gotta_watch_them_all/app/infrastructure/util/ApiRequestBuilder.java
@@ -1,9 +1,14 @@
 package com.gotta_watch_them_all.app.infrastructure.util;
 
+import com.gotta_watch_them_all.app.core.exception.AnySearchValueFoundException;
+import com.gotta_watch_them_all.app.core.exception.IllegalTitleGivenException;
+
 import java.net.http.HttpRequest;
 
 public interface ApiRequestBuilder {
-    HttpRequest build();
+    HttpRequest build() throws AnySearchValueFoundException;
 
-    ApiRequestBuilder setUri(String url);
+    ApiRequestBuilder setUri() throws AnySearchValueFoundException;
+
+    ApiRequestBuilder setTitleToSearch(String title) throws IllegalTitleGivenException;
 }
--- src/test/java/com/gotta_watch_them_all/app/infrastructure/util/ApiRequestBuilderMovieDbTest.java
+++ src/test/java/com/gotta_watch_them_all/app/infrastructure/util/ApiRequestBuilderMovieDbTest.java
@@ -1,13 +1,17 @@
 package com.gotta_watch_them_all.app.infrastructure.util;
 
+import com.gotta_watch_them_all.app.core.exception.AnySearchValueFoundException;
+import com.gotta_watch_them_all.app.core.exception.IllegalTitleGivenException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.http.HttpRequest;
 
@@ -17,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootConfiguration
 @TestPropertySource(properties = {
         "moviedb.api.key=key",
-        "moviedb.api.host=host",
+        "moviedb.api.host=movie-database-imdb-alternative.p.rapidapi.com",
 })
 class ApiRequestBuilderMovieDbTest {
 
@@ -31,13 +35,14 @@ class ApiRequestBuilderMovieDbTest {
 
     private HttpRequest requestModel;
 
-    private final String apiUrl = "https://movie-database.test";
+    private String apiUrl;
 
     @BeforeEach
     public void setup() {
         sut = new ApiRequestBuilderMovieDb();
         ReflectionTestUtils.setField(sut, "apiKey", apiKey);
         ReflectionTestUtils.setField(sut, "apiHost", apiHost);
+        apiUrl = String.format("https://%s", apiHost);
 
         requestModel = HttpRequest.newBuilder()
                 .uri(URI.create(apiUrl))
@@ -48,39 +53,85 @@ class ApiRequestBuilderMovieDbTest {
     }
 
     @Test
-    public void setUri_should_set_uri_with_given_url() {
-        sut.setUri(apiUrl);
-        Object uri = ReflectionTestUtils.getField(sut, "uri");
-        assertEquals(URI.create(apiUrl), uri);
+    public void setTitleToSearch_should_set_title_property() throws IllegalTitleGivenException {
+        sut.setTitleToSearch("titre");
+        Object property = ReflectionTestUtils.getField(sut, "titleToSearch");
+        assertEquals("titre", property);
     }
 
     @Test
-    public void setUri_should_throw_null_pointer_exception_when_given_url_is_null() {
-        assertThrows(NullPointerException.class, () -> sut.setUri(null));
+    public void setTitleToSearch_should_throw_exception_if_title_null() {
+        assertThrows(IllegalTitleGivenException.class, () -> sut.setTitleToSearch(null));
     }
 
     @Test
-    public void build_should_return_http_request_with_right_uri() {
+    public void setTitleToSearch_should_throw_exception_if_title_empty() {
+        assertThrows(IllegalTitleGivenException.class, () -> sut.setTitleToSearch(" "));
+    }
+
+    @Test
+    public void setUri_should_set_base_url_to_start_with_https_plus_api_host() throws MalformedURLException, AnySearchValueFoundException, IllegalTitleGivenException {
+        sut.setTitleToSearch("titre").setUri();
+        URI uri = (URI) ReflectionTestUtils.getField(sut, "uri");
+        String url = uri.toURL().toString();
+        assertTrue(url.startsWith("https://" + apiHost));
+    }
+
+    @Test
+    public void setUri_should_throw_no_search_value_found_if_title_to_search_null() {
+        assertThrows(AnySearchValueFoundException.class, () -> sut.setUri());
+    }
+
+    @Test
+    public void setUri_should_add_title() throws IllegalTitleGivenException, AnySearchValueFoundException, MalformedURLException {
+        sut.setTitleToSearch("titre").setUri();
+        URI uri = (URI) ReflectionTestUtils.getField(sut, "uri");
+        String url = uri.toURL().toString();
+        assertTrue(url.contains("s=titre"));
+    }
+
+    @Test
+    public void setUri_should_add_title_with_special_characters() {
+        assertDoesNotThrow(() -> sut.setTitleToSearch("Les méchants").setUri());
+    }
+
+    @Test
+    public void setUri_should_set_return_type_to_json() throws IllegalTitleGivenException, AnySearchValueFoundException, MalformedURLException {
+        sut.setTitleToSearch("titre").setUri();
+        URI uri = (URI) ReflectionTestUtils.getField(sut, "uri");
+        String url = uri.toURL().toString();
+        assertTrue(url.endsWith("&r=json"));
+    }
+
+    @Test
+    public void build_should_call_set_uri_once() throws AnySearchValueFoundException, IllegalTitleGivenException {
+        ApiRequestBuilderMovieDb spy = Mockito.spy(sut);
+        spy.setTitleToSearch("titre").build();
+        Mockito.verify(spy, Mockito.times(1)).setUri();
+    }
+
+    @Test
+    public void build_should_return_http_request_with_right_uri() throws IllegalTitleGivenException, AnySearchValueFoundException {
         HttpRequest requestFromBuilder = sut
-                .setUri(apiUrl)
+                .setTitleToSearch("titre")
                 .build();
-
-        assertEquals(requestModel.uri(), requestFromBuilder.uri());
+        URI expectedUri = URI.create(String.format("https://%s/?s=%s%s", apiHost, "titre", "&r=json"));
+        assertEquals(expectedUri, requestFromBuilder.uri());
     }
 
     @Test
-    public void build_should_return_http_request_with_right_headers() {
+    public void build_should_return_http_request_with_right_headers() throws IllegalTitleGivenException, AnySearchValueFoundException {
         HttpRequest requestFromBuilder = sut
-                .setUri(apiUrl)
+                .setTitleToSearch("titre")
                 .build();
 
         assertEquals(requestModel.headers(), requestFromBuilder.headers());
     }
 
     @Test
-    public void build_should_return_http_request_with_right_method() {
+    public void build_should_return_http_request_with_right_method() throws IllegalTitleGivenException, AnySearchValueFoundException {
         HttpRequest requestFromBuilder = sut
-                .setUri(apiUrl)
+                .setTitleToSearch("titre")
                 .build();
 
         assertEquals(requestModel.method(), requestFromBuilder.method());

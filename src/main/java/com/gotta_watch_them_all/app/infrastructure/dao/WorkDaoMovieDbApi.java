aae75c1745f1df402aa129d2e039e64a2009f49e
JeremTer <ternisien09@gmail.com>
2021-05-01 01:02:38 +0200

    WorkDaoMovieDbApi Implemented

--- src/main/java/com/gotta_watch_them_all/app/infrastructure/dao/WorkDaoMovieDbApi.java
+++ src/main/java/com/gotta_watch_them_all/app/infrastructure/dao/WorkDaoMovieDbApi.java
@@ -2,12 +2,18 @@ package com.gotta_watch_them_all.app.infrastructure.dao;
 
 import com.gotta_watch_them_all.app.core.dao.WorkDao;
 import com.gotta_watch_them_all.app.core.entity.Work;
+import com.gotta_watch_them_all.app.core.exception.AnySearchValueFoundException;
+import com.gotta_watch_them_all.app.core.exception.BadHttpRequestException;
+import com.gotta_watch_them_all.app.core.exception.IllegalTitleGivenException;
 import com.gotta_watch_them_all.app.infrastructure.util.ApiRequestBuilder;
 import com.gotta_watch_them_all.app.infrastructure.util.ApiRequester;
-import com.gotta_watch_them_all.app.infrastructure.util.WorkParser;
+import com.gotta_watch_them_all.app.infrastructure.util.JsonParser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+
+import java.net.http.HttpRequest;
+import java.util.HashSet;
 import java.util.Set;
 
 @Service
@@ -16,10 +22,19 @@ public class WorkDaoMovieDbApi implements WorkDao {
 
     private final ApiRequestBuilder apiRequestBuilder;
     private final ApiRequester apiRequester;
-    private final WorkParser workParser;
+    private final JsonParser jsonParser;
 
     @Override
     public Set<Work> findAllByTitle(String title) {
-        return null;
+        try {
+            HttpRequest request = apiRequestBuilder
+                    .setTitleToSearch(title)
+                    .build();
+            String jsonRaw = apiRequester.request(request);
+            return new HashSet<>(jsonParser.toObjectList(jsonRaw, Work.class));
+        } catch (AnySearchValueFoundException | IllegalTitleGivenException | BadHttpRequestException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- src/test/java/com/gotta_watch_them_all/app/infrastructure/dao/WorkDaoMovieDbApiTest.java
+++ src/test/java/com/gotta_watch_them_all/app/infrastructure/dao/WorkDaoMovieDbApiTest.java
@@ -1,15 +1,19 @@
 package com.gotta_watch_them_all.app.infrastructure.dao;
 
 import com.gotta_watch_them_all.app.core.entity.Work;
+import com.gotta_watch_them_all.app.core.exception.AnySearchValueFoundException;
+import com.gotta_watch_them_all.app.core.exception.BadHttpRequestException;
+import com.gotta_watch_them_all.app.core.exception.IllegalTitleGivenException;
 import com.gotta_watch_them_all.app.infrastructure.util.ApiRequestBuilder;
 import com.gotta_watch_them_all.app.infrastructure.util.ApiRequester;
-import com.gotta_watch_them_all.app.infrastructure.util.WorkParser;
+import com.gotta_watch_them_all.app.infrastructure.util.JsonParser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,28 +21,107 @@ class WorkDaoMovieDbApiTest {
 
     private WorkDaoMovieDbApi sut;
 
-    @Mock
-    private ApiRequester apiRequester;
-
-    @Mock
-    private ApiRequestBuilder apiRequestBuilder;
-
-    @Mock
-    private WorkParser workParser;
+    private ApiRequester apiRequesterMock;
+    private ApiRequestBuilder apiRequestBuilderMock;
+    private JsonParser jsonParserMock;
 
     @BeforeEach
     public void setup() {
-        sut = new WorkDaoMovieDbApi(apiRequestBuilder, apiRequester, workParser);
+        apiRequesterMock = Mockito.mock(ApiRequester.class);
+        apiRequestBuilderMock = Mockito.mock(ApiRequestBuilder.class);
+        jsonParserMock = Mockito.mock(JsonParser.class);
+        sut = new WorkDaoMovieDbApi(apiRequestBuilderMock, apiRequesterMock, jsonParserMock);
     }
 
     @Test
-    public void find_by_name_should_return_all_works_containing_name() {
-        String title = "Harry Potter";
-        Set<Work> expectedWorks = new HashSet<>();
-        expectedWorks.add(new Work().setId(1L).setTitle("Harry Potter et ceci"));
-        expectedWorks.add(new Work().setId(1L).setTitle("Harry Potter et cela"));
-
-        assertEquals(expectedWorks, sut.findAllByTitle(title));
+    public void findAllByTitle_should_call_api_req_builder_once() throws AnySearchValueFoundException, IllegalTitleGivenException {
+        Mockito.when(apiRequestBuilderMock.setTitleToSearch(Mockito.anyString())).thenReturn(apiRequestBuilderMock);
+        sut.findAllByTitle("title");
+        Mockito.verify(apiRequestBuilderMock, Mockito.times(1)).build();
     }
+
+    @Test
+    public void findAllByTitle_should_call_api_request_builder_set_search_title() throws IllegalTitleGivenException {
+        Mockito.when(apiRequestBuilderMock.setTitleToSearch(Mockito.anyString())).thenReturn(apiRequestBuilderMock);
+        sut.findAllByTitle("bonjour");
+        Mockito.verify(apiRequestBuilderMock, Mockito.times(1)).setTitleToSearch("bonjour");
+    }
+
+    @Test
+    public void findAllByTitle_should_call_api_req_once() throws BadHttpRequestException, IllegalTitleGivenException {
+        Mockito.when(apiRequestBuilderMock.setTitleToSearch(Mockito.anyString())).thenReturn(apiRequestBuilderMock);
+        sut.findAllByTitle("title");
+        Mockito.verify(apiRequesterMock, Mockito.times(1)).request(Mockito.any());
+    }
+
+    @Test
+    public void findAllByTitle_should_call_json_parser_once_with_work_class() throws IllegalTitleGivenException, BadHttpRequestException {
+        Mockito.when(apiRequestBuilderMock.setTitleToSearch(Mockito.anyString())).thenReturn(apiRequestBuilderMock);
+        Mockito.when(apiRequesterMock.request(Mockito.any())).thenReturn("response");
+        sut.findAllByTitle("title");
+        Mockito.verify(jsonParserMock, Mockito.times(1)).toObjectList(Mockito.anyString(), Mockito.any());
+    }
+
+    @Test
+    public void findAllByTitle_should_return_all_works_containing_title() throws IllegalTitleGivenException, AnySearchValueFoundException, BadHttpRequestException {
+        List<Object> expectedWorksList = new ArrayList<>();
+        expectedWorksList.add(new Work().setId(1L).setTitle("Harry Potter et ceci"));
+        expectedWorksList.add(new Work().setId(1L).setTitle("Harry Potter et cela"));
+
+        Set<Work> expectedWorksSet = new HashSet<>();
+        expectedWorksSet.add(new Work().setId(1L).setTitle("Harry Potter et ceci"));
+        expectedWorksSet.add(new Work().setId(1L).setTitle("Harry Potter et cela"));
+
+        Mockito.when(apiRequestBuilderMock.setTitleToSearch(Mockito.anyString())).thenReturn(apiRequestBuilderMock);
+        Mockito.when(apiRequestBuilderMock.build()).thenReturn(HttpRequest.newBuilder().uri(URI.create("https://local")).build());
+        Mockito.when(apiRequesterMock.request(Mockito.any())).thenReturn("response");
+        Mockito.when(jsonParserMock.toObjectList(Mockito.any(), Mockito.any()))
+                .thenReturn(expectedWorksList);
+
+        assertEquals(expectedWorksSet, sut.findAllByTitle("titre"));
+    }
+
+    @Test
+    public void findAllByTitle_should_return_all_works_containing_title_without_duplicates() throws IllegalTitleGivenException, AnySearchValueFoundException, BadHttpRequestException {
+        List<Object> expectedWorksList = new ArrayList<>();
+        expectedWorksList.add(new Work().setId(1L).setTitle("Harry Potter et ceci"));
+        expectedWorksList.add(new Work().setId(1L).setTitle("Harry Potter et ceci"));
+        expectedWorksList.add(new Work().setId(1L).setTitle("Harry Potter et cela"));
+        expectedWorksList.add(new Work().setId(1L).setTitle("Harry Potter et cela"));
+
+        Set<Work> expectedWorksSet = new HashSet<>();
+        expectedWorksSet.add(new Work().setId(1L).setTitle("Harry Potter et ceci"));
+        expectedWorksSet.add(new Work().setId(1L).setTitle("Harry Potter et cela"));
+
+        Mockito.when(apiRequestBuilderMock.setTitleToSearch(Mockito.anyString())).thenReturn(apiRequestBuilderMock);
+        Mockito.when(apiRequestBuilderMock.build()).thenReturn(HttpRequest.newBuilder().uri(URI.create("https://local")).build());
+        Mockito.when(apiRequesterMock.request(Mockito.any())).thenReturn("response");
+        Mockito.when(jsonParserMock.toObjectList(Mockito.any(), Mockito.any()))
+                .thenReturn(expectedWorksList);
+
+        assertEquals(expectedWorksSet, sut.findAllByTitle("titre"));
+    }
+
+    @Test
+    public void findAllByTitle_should_throw_exception_if_json_malformed() throws IllegalTitleGivenException, AnySearchValueFoundException, BadHttpRequestException {
+        List<Object> expectedWorksList = new ArrayList<>();
+        expectedWorksList.add(new Work().setId(1L).setTitle("Harry Potter et ceci"));
+        expectedWorksList.add(new Work().setId(1L).setTitle("Harry Potter et ceci"));
+        expectedWorksList.add(new Work().setId(1L).setTitle("Harry Potter et cela"));
+        expectedWorksList.add(new Work().setId(1L).setTitle("Harry Potter et cela"));
+
+        Set<Work> expectedWorksSet = new HashSet<>();
+        expectedWorksSet.add(new Work().setId(1L).setTitle("Harry Potter et ceci"));
+        expectedWorksSet.add(new Work().setId(1L).setTitle("Harry Potter et cela"));
+
+        Mockito.when(apiRequestBuilderMock.setTitleToSearch(Mockito.anyString())).thenReturn(apiRequestBuilderMock);
+        Mockito.when(apiRequestBuilderMock.build()).thenReturn(HttpRequest.newBuilder().uri(URI.create("https://local")).build());
+        Mockito.when(apiRequesterMock.request(Mockito.any())).thenReturn("response");
+        Mockito.when(jsonParserMock.toObjectList(Mockito.any(), Mockito.any()))
+                .thenReturn(expectedWorksList);
+
+        assertEquals(expectedWorksSet, sut.findAllByTitle("titre"));
+    }
+
 
 }
